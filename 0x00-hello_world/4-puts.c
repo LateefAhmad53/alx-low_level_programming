bd0b9449a039ea1b2dc4017afe53760b4c09ac79
Lateef ahmad <ahmadlateef298@gmail.com>
2022-12-08 15:04:27 -0800

    C puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,5 +6,6 @@
 
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle"); return (0);
+        puts("\"programming is like building a multilingual puzzle");
+                      return (0);
 }
