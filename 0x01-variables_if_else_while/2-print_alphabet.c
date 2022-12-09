ad7218b6012b726f28c87292ef5cad58f911d1cf
Lateef ahmad <ahmadlateef298@gmail.com>
2022-12-09 13:33:25 -0800

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,10 @@
  * */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-putchar(ch);
+	putchar(ch);
 }
 putchar('\n');
 return (0);
