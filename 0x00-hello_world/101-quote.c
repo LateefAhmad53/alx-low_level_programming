531acbeb5fcf6cfcfb576e26a8bd8bf94e75badf
Lateef ahmad <ahmadlateef298@gmail.com>
2022-12-08 16:51:44 -0800

    last C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - print exactly "and that piece of art is useful" 
- * - Dora Korpar, 2015-10-19
- *   followed by a new line, to the standard error.
- *   Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 1
  */
 
 int main(void)
