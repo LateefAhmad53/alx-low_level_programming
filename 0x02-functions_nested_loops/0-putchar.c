8d34f68ada1e451bbf8ff56b23918e15d2664a19
Lateef ahmad <ahmadlateef298@gmail.com>
2022-12-13 09:02:21 -0800

    0 task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,25 @@
-/*
- * File: 0-putchar.c 
- *
- */
+#include <unistd.h>
 
- #include "main.h" 
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
- /** 
- * main-print putchar followed by a new line
- * 
- *Return :always 0
- */
-  int main(void) 
- {
-      _putchar('_' );
-      _putchar ('p' );
-      _putchar ('u' );
-      _putchar ('t' );
-      _putchar ('c' );
-      _putchar ('h' );
-      _putchar ('a' );
-      _putchar ('r' );
-      _putchar ('\n');
-      
-       return (0) ;
-} 
+int main ()
+{
+
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+
+return 0;
+
+
+}
