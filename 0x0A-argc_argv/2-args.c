a174eb94aec0857cafe9ad9b23d66f724b74a1b5
Lateef ahmad <ahmadlateef298@gmail.com>
2023-01-06 06:38:47 -0800

    task 3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints all arguments it receives.
+ * @argc: size of argv .
+ * @argv: a pointer to the array containing the program line arguments .
+ * Return: 0 .
+ */
+int main(int argc, char *argv[])
+{
+
+while (*argv)
+{
+(void) argc;
+printf("%s\n", *argv);
+argv++;
+}
+return (0);
+
+}
