77ebb1ca1dc5fe0a4b196cfa18e01b28f8d92adf
Lateef ahmad <ahmadlateef298@gmail.com>
2023-01-06 06:37:02 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints the number of arguments passed into it.
+ * @argc: size of argv
+ * @argv: array containing the program line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+
+}
